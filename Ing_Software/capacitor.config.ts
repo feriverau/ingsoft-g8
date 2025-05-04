62992c8ab620c229ad83e7befbe34aff980d38d3
GustavoValdesZ <gustavovaldes.z@gmail.com>
2025-05-04 01:53:32 -0400

    Delete Ing_Software directory
    carpeta innecesaria

--- Ing_Software/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Ing_Software',
-  webDir: 'www'
-};
-
-export default config;
